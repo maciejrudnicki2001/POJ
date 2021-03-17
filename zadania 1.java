cc80bf9ed6f43fa73323845a999530b7b1664b27
maciejrudnicki2001 <s22983@pjwstk.edu.pl>
2021-03-17 08:27:33 +0100

    Debug ex2

--- zadania 1.java
+++ zadania 1.java
@@ -39,22 +39,31 @@ public class pr_2 {
                 break;
             case 5:
                 System.out.println("May");
+                break;
             case 6:
                 System.out.println("June");
+                break;
             case 7:
                 System.out.println("July");
+                break;
             case 8:
                 System.out.println("August");
+                break;
             case 9:
                 System.out.println("September");
+                break;
             case 10:
                 System.out.println("October");
+                break;
             case 11:
                 System.out.println("November");
+                break;
             case 12:
                 System.out.println("December");
+                break;
             default:
                 System.out.println("Nie oszukuj pls...");
+                break;
         }
 
     }
