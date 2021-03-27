8054d199731313952fcf3a7fda9143d8140f16c6
maciejrudnicki2001 <75856604+maciejrudnicki2001@users.noreply.github.com>
2021-03-27 16:22:23 +0100

    exercise2

--- LAB2/zad_2.java
+++ LAB2/zad_2.java
@@ -1,29 +1,31 @@
 package pjatk;
-import java.util.Arrays;
-import java.util.Scanner;
-import java.util.Random;
-public class pr_7 {
 
+import java.util.Arrays;
+import java.util.Random;
+
+public class SecondExercise {
     public static void main(String[] args) {
         Random rand = new Random();
-        int c = rand.nextInt(10);
-        int[] Tablica = new int[c];
-        int[] Tablica2 = new int[c];
-        int[] Tablica3 = new int[c];
-        int i = 0;
-        while(i < c){
-            //Wprowadziłem generowanie liczb
-            Random rand1 = new Random();
-            int a = rand.nextInt(50);
-            int b = rand.nextInt(50);
-            Tablica[i] = a;
-            Tablica2[i] = b;
-            Tablica3[i] = a+b;
-            i++;
+        int array_size = rand.nextInt(5);
+        int[] array1 = new int[array_size];
+        int[] array2  = new int[array_size];
+        int[] array3;
+        for (int counter = 0; counter < array_size; counter++){
+            int value_array1 = rand.nextInt(50);
+            int value_array2 = rand.nextInt(50);
+            array1[counter] = value_array1;
+            array2[counter] = value_array2;
         }
-        System.out.println("Tablica1: "+Arrays.toString(Tablica));
-        System.out.println("Tablica2: "+Arrays.toString(Tablica2));
-        System.out.println("Tablica3: "+Arrays.toString(Tablica3));
-
+        if (array1.length != array2.length){
+            array3 = new int[0];
+            System.out.print(Arrays.toString(array3));
+        }
+        else {
+            array3 = new int[array1.length];
+            for (int counter2 = 0; counter2 < array_size; counter2++){
+                array3[counter2] = array1[counter2] + array2[counter2];
+            }
+            System.out.print(Arrays.toString(array3));
+        }
     }
 }
