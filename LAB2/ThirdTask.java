d737a1f36cfef2e3abd69efa3cd94fe2afad020f
maciejrudnicki2001 <75856604+maciejrudnicki2001@users.noreply.github.com>
2021-03-27 17:20:04 +0100

    exercise 3

--- LAB2/ThirdTask.java
+++ LAB2/ThirdTask.java
@@ -1,20 +1,13 @@
 package pjatk;
 
 public class ThirdTask {
-    public static void main(String[] args) {
-
-        int[][] array1 = {{23,34,22},{15,6,24},{3,4,5}};
-        int[][] array2 = {{50,2,3},{3,4,5},{3,4,5}};
-
+    public static int sum(int[][] array, int[][] array2){
         int sum = 0;
-        for (int counter1 = 0; counter1 < array1.length; counter1++)
-        {
-            for (int counter2 = 0; counter2 < array1[counter1].length; counter2++)
-            {
-                sum = sum + array1 [counter1][counter2] + array2[counter1][counter2];
+        for (int counter1 = 0; counter1 < array.length; counter1++){
+            for (int counter2 = 0; counter2 < array.length; counter2++){
+                sum = sum + array[counter1][counter2]+ array2[counter1][counter2];
             }
         }
-
-        System.out.println("Sum = " + sum);
+        return sum;
     }
 }
