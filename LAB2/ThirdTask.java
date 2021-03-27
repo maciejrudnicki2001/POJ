1c74bcb1475f8f253b9ac42d49bce460d13d3be1
maciejrudnicki2001 <75856604+maciejrudnicki2001@users.noreply.github.com>
2021-03-27 17:01:18 +0100

    Update ThirdTask.java

--- LAB2/ThirdTask.java
+++ LAB2/ThirdTask.java
@@ -7,9 +7,9 @@ public class ThirdTask {
         int[][] array2 = {{50,2,3},{3,4,5},{3,4,5}};
 
         int sum = 0;
-        for (int counter1 = 0;counter1 < array1.length;counter1++)
+        for (int counter1 = 0; counter1 < array1.length; counter1++)
         {
-            for (int counter2 = 0;counter2 < array1[counter1].length;counter2++)
+            for (int counter2 = 0; counter2 < array1[counter1].length; counter2++)
             {
                 sum = sum + array1 [counter1][counter2] + array2[counter1][counter2];
             }
