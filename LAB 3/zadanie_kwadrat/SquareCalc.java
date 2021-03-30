8034081b87ce34aabaddebc339d99982a6267164
maciejrudnicki2001 <75856604+maciejrudnicki2001@users.noreply.github.com>
2021-03-30 20:09:22 +0200

    Update SquareCalc.java

--- LAB 3/zadanie_kwadrat/SquareCalc.java
+++ LAB 3/zadanie_kwadrat/SquareCalc.java
@@ -4,17 +4,11 @@ public class SquareCalc {
 
         Side side = new Side(20);
 
-        System.out.println("Area:" + AreaCalc(side.side));
+        System.out.println("Area:" + Side.AreaCalc(side.side));
 
-        System.out.println("Perimeter:" + PerimeterCalc(side.side));
+        System.out.println("Perimeter:" + Side.PerimeterCalc(side.side));
 
     }
-    public static int AreaCalc(int side){
-        int area = side * 2;
-        return area;
-    }
-    public static int PerimeterCalc(int side){
-        int perimeter = side * 4;
-        return perimeter;
-    }
+
+
 }
