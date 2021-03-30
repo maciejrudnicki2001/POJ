49de0f3d79af17991cfec2a4f30316e07356c2f8
maciejrudnicki2001 <75856604+maciejrudnicki2001@users.noreply.github.com>
2021-03-30 20:09:56 +0200

    Update Side.java

--- LAB 3/zadanie_kwadrat/Side.java
+++ LAB 3/zadanie_kwadrat/Side.java
@@ -1,10 +1,21 @@
 package pjatk.zadanie_kwadrat;
 
 public class Side {
+    
+        int side;
 
-    int side;
+        Side(int a) {
+            side = a;
+        }
 
-    Side(int a) {
-        side = a;
+    public static int AreaCalc(int side){
+        int area = side * 2;
+        return area;
     }
+    public static int PerimeterCalc(int side){
+        int perimeter = side * 4;
+        return perimeter;
+    }
+
+
 }
