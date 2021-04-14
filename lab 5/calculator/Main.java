363350852156c5e70ed5847fb38acbbc6b24a402
maciejrudnicki2001 <75856604+maciejrudnicki2001@users.noreply.github.com>
2021-04-14 11:54:41 +0200

    Update Main.java

--- lab 5/calculator/Main.java
+++ lab 5/calculator/Main.java
@@ -2,7 +2,7 @@ package pjatk.calculator;
 
 public class Main {
     public static void main(String[] args) {
-        Calculator calc = new Calculator(1,2,3,4,5,6);
+        Calculator calc = new Calculator();
         System.out.println(calc.calculate(1));
         System.out.println(calc.calculate(2,3));
         System.out.println(calc.calculate(4,5,6));
