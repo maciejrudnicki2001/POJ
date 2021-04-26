cef6a92a056f6b9db985347bb0da508675cb923a
maciejrudnicki2001 <s22983@pjwstk.edu.pl>
2021-04-26 22:01:56 +0200

    yup

--- LAB6/date/Date.java
+++ LAB6/date/Date.java
@@ -1,28 +1,19 @@
 package pjatk.date;
-
-
+import java.text.DateFormatSymbols;
 public class Date {
     private final int days;
-    public final int months;
+    private final int months;
     private final int years;
 
-
-    public Date(final int days, final int months, final int years) {
+    public Date(final int days, final int months, final int years){
         this.days = days;
         this.months = months;
         this.years = years;
-
     }
-
-    //@Override
-    //public String toString() {
-      //  return (this.days+"."+ this.months+"."+ this.years);
-    //}
-
-
-    @Override
     public String toString() {
-        final Month month = new Month(this.months);
-        return (this.days+"\t"+ month +this.years);
+        return this.days +"."+ this.months +"."+ this.years;
+    }
+    public String toString1() {
+        return this.days+" "+  new DateFormatSymbols().getMonths()[this.months - 1]+" "+ this.years;
     }
 }
--- LAB6/date/Main.java
+++ LAB6/date/Main.java
@@ -1,9 +1,9 @@
 package pjatk.date;
 
 public class Main {
-    public static void main(final String[] args) {
-        final Date date = new Date(1, 2, 2000);
-
-        System.out.println(date);
+    public static void main(String[] args) {
+        final Date date = new Date(4,6,2001);
+        System.out.println(date.toString());
+        System.out.println(date.toString1());
     }
 }
